b2761e14a9dd7fd37a2553e6815801aac3a6c526
ForrestGR <f.brede@hotmail.de>
2024-04-02 23:29:32 +0200

    update

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+
+    public event EventHandler OnInteractAction;
+
     private PlayerInputActions playerInputActions;
 
 
@@ -11,6 +15,21 @@ public class GameInput : MonoBehaviour
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+            OnInteractAction?.Invoke(this, EventArgs.Empty);                                           
+            Debug.Log(obj);
+
+
+        //das gleiche wie mit ?.Invoke
+        //if (OnInteractAction != null)
+        //{
+        //    OnInteractAction(this, EventArgs.Empty);
+        //}
     }
 
 
